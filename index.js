649c0f7d7a35ddaab8d578b6660831fd15fb5bc6
Enoch Klufio <enochmk94@gmail.com>
2021-06-13 14:09:06 +0000

    Refined

--- index.js
+++ index.js
@@ -9,17 +9,16 @@ const errorHandler = require('./middleware/error');
 const app = express();
 
 app.use(cors());
-process.env.NODE_ENV = 'development' && app.use(morgan('tiny'));
+app.use(morgan('tiny'));
 app.use(xmlparser());
 
 app.use('/biometric-agent', require('./routes/biometric-agent'));
-
-// error handler
 app.use(errorHandler);
 
 // Set server port to listen
 const PORT = process.env.NODE_PORT;
 
-const MESSAGE = `Agent BIO started in ${process.env.NODE_ENV} on port:${PORT}`;
+const MESSAGE = `BIOMETRIC AGENT USSD API started in ${process.env.NODE_ENV} on port: ${PORT}`;
+
 
 app.listen(PORT, () => console.log(MESSAGE));
