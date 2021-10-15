a7a977754341ec6671f67a2e117226fe7eb13c62
Enoch Klufio <enochmk94@gmail.com>
2021-10-15 16:57:36 +0000

    MFS Registration Complete

--- api/mfsRegistration.js
+++ api/mfsRegistration.js
@@ -1,41 +1,46 @@
 const axios = require('axios');
-const Logger = require('../../utils/Logger');
-const formatGhanaCard = require('../../utils/formatGhanaCard');
+const util = require('util');
 
-const action = async (requestID, agentID, answers, cellID) => {
+const Logger = require('../utils/Logger');
+
+const action = async (requestID, agentID, answers, cellID = null) => {
 	const data = {
 		requestID: requestID,
-		agentID: agentID,
 		cellID: cellID,
-		msisdn: answers[1],
-		nationalID: formatGhanaCard(answers[2].toUpperCase()),
-		forenames: answers[3].toUpperCase(),
-		surname: answers[4].toUpperCase(),
-		gender: answers[5] == '1' ? 'MALE' : 'FEMALE',
-		dateOfBirth: answers[6],
-		nextOfKin: answers[7].toUpperCase(),
+		agentID: agentID,
+		msisdn: answers.MSISDN,
+		nationalID: answers.ID,
+		forenames: answers.FORENAMES,
+		surname: answers.SURNAME,
+		gender: answers.SEX,
+		dateOfBirth: answers.DOB,
+		nextOfKin: answers.NOK.toUpperCase(),
 		channelID: 'ussd',
 	};
 
-	Logger(
-		`${requestID}|${agentID}|API|nonBioRegistrationMfsAPI|request|${JSON.stringify(
+	try {
+		const response = await axios.post(
+			process.env.RE_REGISTRATION_MFS_URL,
 			data
-		)}`
-	);
+		);
 
-	const response = await axios.post(
-		process.env.NON_BIO_REGISTRATION_MFS_URL,
-		data
-	);
-
-	Logger(
-		`${requestID}|${agentID}|API|nonBioRegistrationMfsAPI|response|${JSON.stringify(
-			{
-				cellID: cellID,
-				response: response.data,
-			}
-		)}`
-	);
+		Logger(
+			`${requestID}|${agentID}|API|nonBioRegistrationMFSAPI|response|${JSON.stringify(
+				{ request: data, response: response.data }
+			)}`
+		);
+	} catch (error) {
+		Logger(
+			`${requestID}|${agentID}|API|nonBioRegistrationMFSAPI|response|${JSON.stringify(
+				{
+					request: data,
+					error: error.response ? error.response.data : error.message,
+					message: error.message,
+					stack: util.inspect(error.stack),
+				}
+			)}`
+		);
+	}
 };
 
 module.exports = action;
--- functions/option2.js
+++ functions/option2.js
@@ -1,7 +1,11 @@
+const moment = require('moment');
+
 const Menu = require('../menu.json');
 const Messages = require('../utils/Messages.json');
 const Session = require('../models/Session');
+const formatGhanaCard = require('../utils/formatGhanaCard');
 const sendXMLResponse = require('../utils/XMLResponse');
+const registrationMFSAPI = require('../api/mfsRegistration');
 
 module.exports = async (option, sessionID, msisdn, starcode, timestamp) => {
 	const questions = Menu[option];
@@ -10,21 +14,69 @@ module.exports = async (option, sessionID, msisdn, starcode, timestamp) => {
 	let nextIndex = 0;
 	let nextQuestion = null;
 	let endSession = false;
+	let data = {};
+	let lastSession = {
+		page: null,
+		answer: null,
+		question: null,
+	};
 
+	// get all sessions for this MSISDN
 	const sessions = await Session.find({ msisdn: msisdn, option: option }).sort({
 		_id: 'desc',
 	});
 
-	if (sessions.length) {
-		const lastSession = sessions[0];
+	// init an array of answers in desc order
+	const answers = sessions.map((session) => session.answer);
 
+	// this MSISDN has session, use the latest session
+	if (sessions.length) {
+		lastSession = sessions[0];
 		currentIndex = keys.indexOf(lastSession.page);
 		nextIndex = currentIndex + 1;
 	}
 
+	/************************************************************************
+	 **@description Validate
+	 ***********************************************************************/
+	const validateResponse = await validateLastSession(lastSession, nextIndex);
+	if (validateResponse.error === true) {
+		return sendXMLResponse(
+			sessionID,
+			msisdn,
+			starcode,
+			validateResponse.question,
+			1,
+			timestamp
+		);
+	}
+
+	/************************************************************************/
+
 	// Check if there's a next question else finish
 	if (questions[keys[nextIndex]]) {
 		nextQuestion = questions[keys[nextIndex]];
+
+		// confirm
+		if (keys[nextIndex] === 'confirm') {
+			answers.reverse();
+
+			data.MSISDN = answers[0];
+			data.ID = answers[1];
+			data.FORENAMES = answers[2].toUpperCase();
+			data.SURNAME = answers[3].toUpperCase();
+			data.SEX = answers[4] === '1' ? 'MALE' : 'FEMALE';
+			data.DOB = answers[5];
+			data.NOK = answers[6].toUpperCase();
+
+			nextQuestion = nextQuestion.replace('(MSISDN)', data.MSISDN);
+			nextQuestion = nextQuestion.replace('(ID)', data.ID);
+			nextQuestion = nextQuestion.replace('(FORENAMES)', data.FORENAMES);
+			nextQuestion = nextQuestion.replace('(SURNAME)', data.SURNAME);
+			nextQuestion = nextQuestion.replace('(SEX)', data.SEX);
+			nextQuestion = nextQuestion.replace('(DOB)', data.DOB);
+			nextQuestion = nextQuestion.replace('(NOK)', data.NOK);
+		}
 
 		// ask the next question
 		await Session.create({
@@ -40,7 +92,28 @@ module.exports = async (option, sessionID, msisdn, starcode, timestamp) => {
 	// No more questions
 	if (!questions[keys[nextIndex]]) {
 		endSession = true;
-		nextQuestion = Menu['finish'];
+		nextQuestion =
+			lastSession.answer === '1' ? Menu['finish'] : Messages.onCancel;
+
+		await Session.deleteMany({ msisdn: msisdn });
+	}
+
+	if (lastSession.page === 'confirm') {
+		// * call API;
+		if (lastSession.answer === '1') {
+			answers.reverse();
+
+			data.MSISDN = answers[0];
+			data.ID = answers[1];
+			data.FORENAMES = answers[2].toUpperCase();
+			data.SURNAME = answers[3].toUpperCase();
+			data.SEX = answers[4] === '1' ? 'MALE' : 'FEMALE';
+			data.DOB = answers[5];
+			data.NOK = answers[6].toUpperCase();
+
+			registrationMFSAPI(sessionID, msisdn, data, null);
+		}
+
 		await Session.deleteMany({ msisdn: msisdn });
 	}
 
@@ -52,4 +125,70 @@ module.exports = async (option, sessionID, msisdn, starcode, timestamp) => {
 		endSession ? 2 : 1,
 		timestamp
 	);
+};
+
+/**
+ * @description: Function to validate the last Session's Input
+ */
+const validateLastSession = async (lastSession) => {
+	// ! Validate Customer's MSISDN
+	if (lastSession.page === '1') {
+		let customerMsisdn = lastSession.answer;
+		customerMsisdn = customerMsisdn.substr(customerMsisdn.length - 9);
+		if (customerMsisdn.length !== 9) {
+			return {
+				error: true,
+				question: `Invalid MSISDN, try again.\n${lastSession.question.toString()}`,
+			};
+		}
+	}
+
+	// ! Validate NationalID
+	if (lastSession.page === '2') {
+		const nationalID = formatGhanaCard(lastSession.answer);
+		if (!nationalID) {
+			return {
+				error: true,
+				question: `Invalid pin number, try again.\n${lastSession.question.toString()}`,
+			};
+		}
+	}
+
+	// ! Validate gender
+	if (lastSession.page === '5') {
+		const genderID = lastSession.answer;
+		if (!['1', '2'].includes(genderID)) {
+			return {
+				error: true,
+				question: `Invalid option, try again.\n${lastSession.question.toString()}`,
+			};
+		}
+	}
+
+	// ! Validate dateOfBirth
+	if (lastSession.page === '6') {
+		const dob = lastSession.answer;
+		if (dob.length !== 8) {
+			return {
+				error: true,
+				question: `Invalid format, try again.\n${lastSession.question.toString()}`,
+			};
+		}
+	}
+
+	// ! confirmation
+	if (lastSession.page === 'confirm') {
+		const confirm = lastSession.answer;
+		if (!['1', '2'].includes(confirm)) {
+			return {
+				error: true,
+				question: `Invalid option, try again.\n${lastSession.question.toString()}`,
+			};
+		}
+	}
+
+	return {
+		error: false,
+		question: null,
+	};
 };
